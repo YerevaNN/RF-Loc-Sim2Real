ed3a1321af913f1925aaef5c2bddb0b6149ec4df
AraratSaribekyan <ararat.saribekyan.01.09@gmail.com>
2025-07-08 18:23:57 +0400

    Added WSD scheduler

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from src.utils.metrics import dice_loss
-from src.utils.schedulers import LinearWarmupCosineAnnealingLR
+from src.utils.schedulers import LinearWarmupCosineAnnealingLR, WarmupStableDecayLR
 from src.utils.utils import (
     CompileParams, EpochCounter, log_hyperparameters,
     print_config, ProgressBarTheme, set_winsize, unpatch, worker_initializer,
--- src/utils/schedulers.py
+++ src/utils/schedulers.py
@@ -58,3 +58,46 @@ class LinearWarmupCosineAnnealingLR(LRScheduler):
             ))
             for _ in self.base_lrs
         ]
+
+
+class WarmupStableDecayLR(LRScheduler):
+    def __init__(
+        self,
+        optimizer: Optimizer,
+        warmup_epochs: int,
+        stable_epochs: int,
+        max_epochs: int,
+        warmup_start_lr: float,
+        stable_lr: float,
+        eta_min: float,
+        last_epoch: int,
+    ) -> None:
+        
+        self.warmup_epochs = warmup_epochs
+        self.stable_epochs = stable_epochs
+        self.warmup_start_lr = warmup_start_lr
+        self.warmup_end_lr = stable_lr
+        self.stable_lr = stable_lr
+        self.decay_start_lr = stable_lr
+        self.decay_end_lr = eta_min
+        self.max_epochs = max_epochs
+
+        super().__init__(optimizer, last_epoch)
+    
+    def get_lr(self) -> List[float]:
+        if self.last_epoch < self.warmup_end_lr:
+            return [
+                self.warmup_start_lr
+                + (self.warmup_end_lr - self.warmup_start_lr)
+                * (self.last_epoch / (self.warmup_epochs - 1))
+                for _ in self.base_lrs
+            ]
+        elif self.last_epoch < self.stable_epochs:
+            return [self.stable_lr for _ in self.base_lrs]
+        else:
+            return [
+                self.decay_start_lr
+                - (self.decay_start_lr - self.decay_end_lr)
+                * (self.last_epoch - self.stable_epochs) / (self.max_epochs - self.stable_epochs)
+                for _ in self.base_lrs
+            ]
